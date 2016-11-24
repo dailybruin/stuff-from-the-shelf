3579f273b7e7d09425e94b331de46a2c6735e8b6
Paulina Lei <paulina.y.lei@gmail.com>
2016-11-24 00:16:20 -0800

    initial commit

---
+++ JS/application.js
@@ -0,0 +1,47 @@
+ // (function() {
+            
+ //            var windowH = $(window).height(),
+ //                documElem = $(document),
+ //                landingPage = $('.landingPage'),
+ //                article = $('.article'),
+ //                articleContent= $('#articleContent'),
+ //                Headline= $('#Headline'),
+ //                albumCover= $('#albumCover'),
+ //                playlist= $('#playlist'),
+ //                btns = $('.btn'),
+ //                animSpeed = 500;
+ //            var article1Headline= "An Argument With Myself";
+ //            landingPage.css({
+ //                height: windowH + 'px',
+ //                top: 0 + 'px'
+ //            });
+
+ //            article.css({
+ //                top: windowH + 'px',
+ //            });
+ //           $('#article1').on('click', function(e) {
+ //                    playlist.load("Assets/Multimedia/article1playlist.html")
+ //                    Headline.html(article1Headline);
+ //                    albumCover.load("Assets/Art/article1image.html");
+ //                    articleContent.load("Assets/articleText/article1.html");
+
+ //                    landingPage.animate({'top': -windowH + 'px'}, {duration: animSpeed, queue: false});
+ //                    article.animate({'top': 0},{duration: animSpeed, queue: false} );
+ //           });
+
+ //        btns.on('click', function(e) {
+ //                if ( $(this).hasClass('open') ) {
+ //                    article.animate({'top': windowH + 'px'}, {duration: animSpeed, queue: false});
+ //                    landingPage.animate({'top': 0}, {duration: animSpeed, queue: false});
+ //                }
+ //                e.preventDefault();
+ //            });
+            
+            
+            
+            
+            
+ //        })();
+        
+        
+   
---
+++ README.md
@@ -0,0 +1,2 @@
+# Stuff_from_the_shelf
+A flatpage collection of "Stuff from the Shelf" columns from Daily Bruin's Arts and Entertainment Department
